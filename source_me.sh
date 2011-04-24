33b14fc8005bedae5ae615458cae1829e171e6e9
Ido Rosen <github@idorosen.com>
2011-04-23 20:41:04 -0400

    Added NLTK data sets and comments.

--- source_me.sh
+++ source_me.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
+# clone or pull the virtualenv tools from their git repository.
+if [ ! -f virtualenv/virtualenv.py ]; then
+  git submodule init
+  git submodule update
+fi
+
+# If a virtual environment does not exist, create one so we don't need to
+# litter Python modules all over the system.
 if [ ! -f env/bin/activate ]; then
-  if [ ! -f virtualenv/virtualenv.py ]; then
-    git submodule update --init
-  fi
   python virtualenv/virtualenv.py env
 fi
 
+# Enter the virtual environment we created above or previously...
 source env/bin/activate
 
+# I realize this seems unnecessarily complicated, but it's necessary
+# since the NLTK.org download link is broken and the tarball does not
+# explicitly identify PyYAML as a dependency if it's used directly...
 pip install pyyaml
 pip install -r requirements.txt
+
+# Download NLTK datasets.
+python -m nltk.downloader all
